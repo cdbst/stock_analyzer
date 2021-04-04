fc8895097445811d7547d5924c277b66d8e3ed7b
cdbst <parkhyeonwook@gmail.com>
2021-04-05 02:07:49 +0900

    api server 샘플 추가

--- server.js
+++ server.js
@@ -12,3 +12,26 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 8080, function () {
     console.log('run -> stock analyzer API server ' + (process.env.PORT || 8080));
 });
+
+/**
+ * @description 'rest api' -  'getthreqexp' : 타이틀 홀더 카드의 특정 레벨 달성을 위해 필요한 기존카드의 최소 경험치를 계산함.
+ */
+ api_router.post('/getthreqexp', function (req, res){
+
+    if(req.body.action == null && req.body.action.params == null){
+        res_to_kakao_server(res, "invalid server error (invalid client request)");
+        return;      
+    }
+
+    const tiker = req.body.action.params.tiker; // title holder card level
+});
+
+
+var res_to_kakao_server = function (_res, _msg){
+
+    const res_body = {
+        result : _msg
+    };
+
+    _res.status(200).send(res_body);   
+}
